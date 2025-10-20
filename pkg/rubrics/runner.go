2dcff2b16b60f40e7ccfc2ae1eb8a22ac60e87bf
Jacob Hochstetler <jacob.hochstetler@gmail.com>
2025-10-20 18:28:56 -0500

    lint fix

--- pkg/rubrics/runner.go
+++ pkg/rubrics/runner.go
@@ -73,7 +73,7 @@ type ExecCommandFactory struct {
 
 // New creates a new execCmd wrapper.
 //
-
+//nolint:ireturn // execCmd implements Commander
 func (f *ExecCommandFactory) New(name string, arg ...string) Commander {
 	return &execCmd{Cmd: exec.CommandContext(f.Context, name, arg...)}
 }
